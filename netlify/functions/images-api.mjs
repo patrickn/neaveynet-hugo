f05b4a04aa16b7bcfd3466a473850e671b37194d
Patrick <patrick@neavey.net>
2025-03-11 15:55:37 +0100

    Move api script to .mjs

--- netlify/functions/images-api.mjs
+++ netlify/functions/images-api.mjs
@@ -1,11 +1,11 @@
-exports.handler = async () => {
+export async function handler() {
     const images = [
         { url: "/images/photo1.jpg", title: "Photo 1" },
         { url: "/images/photo2.jpg", title: "Photo 2" }
     ];
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ images }),
+
+    return new Response(JSON.stringify({ images }), {
+        status: 200,
         headers: { "Content-Type": "application/json" }
-    };
-};
+    });
+}
